a665c5e3b5ad6d3956146af43138bfa04f108604
Al-Abir <sheikhabir196@gmail.com>
2024-10-22 20:39:15 +0600

    Client-side

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,12 +9,14 @@ import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
 import Secret from "../pages/Shared/Secret/Secret";
 import PrivateRoute from "./PrivateRoute";
-import Dashbord from "../Layout/Dashbord";
+import Dashbord from "../Layout/Dashboard";
 import Cart from "../pages/Dashbord/Cart/Cart";
 import AllUsers from "../pages/Dashbord/AllUsers/AllUsers";
 import AddItems from "../pages/Dashbord/AddItems.jsx/AddItems";
 import AdminRoute from "./AdminRoute";
 import ManageItems from "../pages/Dashbord/ManageItems/ManageItems";
+import UpdateItem from "../pages/Dashbord/UpdateItem/UpdateItem";
+import Dashboard from "../Layout/Dashboard";
 
 
   export const router = createBrowserRouter([
@@ -34,41 +36,49 @@ import ManageItems from "../pages/Dashbord/ManageItems/ManageItems";
           path: 'order/:category',
           element: <Order></Order>
         },
-         {
-          path:'/login',
-          element:<Login></Login>
-         },{
+        {
+          path: 'login',
+          element: <Login></Login>
+        },
+        {
           path: 'signup',
           element: <SignUp></SignUp>
-         },
-         {
+        },
+        {
           path: 'secret',
-          element:<PrivateRoute><Secret></Secret></PrivateRoute>
-         }
+          element: <PrivateRoute><Secret></Secret></PrivateRoute>
+        }
       ]
     },
     {
-      path:'dashboard',
-      element:  <PrivateRoute> <Dashbord></Dashbord></PrivateRoute>,
+      path: 'dashboard',
+      element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
       children: [
-        //normal user routes
+        // normal user routes
         {
           path: 'cart',
-          element:<Cart></Cart>
+          element: <Cart></Cart>
         },
-        // admin Routes
+
+        // admin only routes
         {
-          path:'addItems',
-          element:<AdminRoute> <AddItems></AddItems>  </AdminRoute>
+          path: 'addItems',
+          element: <AdminRoute><AddItems></AddItems></AdminRoute>
         },
         {
-            path:'manageItems',
-            element:<AdminRoute><ManageItems></ManageItems></AdminRoute>
+          path: 'manageItems',
+          element: <AdminRoute><ManageItems></ManageItems></AdminRoute>
+        },
+        {
+          path: 'updateItem/:id',
+          element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
+          loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
         },
         {
           path: 'users',
-          element: <AllUsers></AllUsers>
+          element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
         }
+
       ]
     }
   ]);
