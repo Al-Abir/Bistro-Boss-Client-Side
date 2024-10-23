62f9dda8eed7775736dd75ed771514e017f7c4d4
Al-Abir <sheikhabir196@gmail.com>
2024-10-23 15:24:51 +0600

    client-side

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -6,42 +6,95 @@ import useAdmin from "../hooks/useAdmin";
 
 
 const Dashboard = () => {
-    const user = { email: "currentUserEmail" }; // Replace this with actual user authentication context.
-    const [isAdmin, loading] = useAdmin(user.email);
-    const [cart] = Usecart();
+    const [cart] = Usecart()
 
-    if (loading) {
-        return <p>Loading...</p>; // Optional: show loading state while fetching
-    }
+    // TODO: get isAdmin value from the database
+    const [isAdmin] = useAdmin();
 
     return (
         <div className="flex">
-            <div className="w-64 min-h-screen bg-amber-950">
+            {/* dashboard side bar */}
+            <div className="w-64 min-h-screen bg-red-900">
                 <ul className="menu p-4">
-                    {isAdmin ? (
-                        <>
-                            <li><NavLink to="/dashboard/adminHome"><FaHome />Admin Home</NavLink></li>
-                            <li><NavLink to="/dashboard/addItems"><FaUtensils />Add Items</NavLink></li>
-                            <li><NavLink to="/dashboard/manageItems"><FaList />Manage Items</NavLink></li>
-                            <li><NavLink to="/dashboard/bookings"><FaBook />Manage Bookings</NavLink></li>
-                            <li><NavLink to="/dashboard/users"><FaUsers />All Users</NavLink></li>
+                    {
+                        isAdmin ? <>
+                            <li>
+                                <NavLink to="/dashboard/adminHome">
+                                    <FaHome></FaHome>
+                                    Admin Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/addItems">
+                                    <FaUtensils></FaUtensils>
+                                    Add Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/manageItems">
+                                    <FaList></FaList>
+                                    Manage Items</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/bookings">
+                                    <FaBook></FaBook>
+                                    Manage Bookings</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/dashboard/users">
+                                    <FaUsers></FaUsers>
+                                    All Users</NavLink>
+                            </li>
                         </>
-                    ) : (
-                        <>
-                            <li><NavLink to="/dashboard/userHome"><FaHome />User Home</NavLink></li>
-                            <li><NavLink to="/dashboard/paymentHistory"><FaCalendar />Payment History</NavLink></li> {/* Update here */}
-                            <li><NavLink to="/dashboard/cart"><FaShoppingCart />My Cart ({cart.length})</NavLink></li>
-                            <li><NavLink to="/dashboard/review"><FaAd />Add a Review</NavLink></li>
-                        </>
-                    )}
+                            :
+                            <>
+                                <li>
+                                    <NavLink to="/dashboard/userHome">
+                                        <FaHome></FaHome>
+                                        User Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/history">
+                                        <FaCalendar></FaCalendar>
+                                        Not History</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/cart">
+                                        <FaShoppingCart></FaShoppingCart>
+                                        My Cart ({cart.length})</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/review">
+                                        <FaAd></FaAd>
+                                        Add a Review</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/dashboard/paymentHistory">
+                                        <FaList></FaList>
+                                        Real Payment History</NavLink>
+                                </li>
+                            </>
+                    }
+                    {/* shared nav links */}
                     <div className="divider"></div>
-                    <li><NavLink to="/"><FaHome />Home</NavLink></li>
-                    <li><NavLink to="/order/salad"><FaSearch />Menu</NavLink></li>
-                    <li><NavLink to="/order/contact"><FaEnvelope />Contact</NavLink></li>
+                    <li>
+                        <NavLink to="/">
+                            <FaHome></FaHome>
+                            Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/salad">
+                            <FaSearch></FaSearch>
+                            Menu</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/order/contact">
+                            <FaEnvelope></FaEnvelope>
+                            Contact</NavLink>
+                    </li>
                 </ul>
             </div>
+            {/* dashboard content */}
             <div className="flex-1 p-8">
-                <Outlet />
+                <Outlet></Outlet>
             </div>
         </div>
     );
